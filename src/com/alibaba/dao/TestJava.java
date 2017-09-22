704a2b60c763d3c0f15110729fd8f6c6eea0c278
朱金东 <@DESKTOP-HNNIUA2.lan>
2017-09-22 18:44:40 +0800

    测试

---
+++ src/com/alibaba/dao/TestJava.java
@@ -0,0 +1,10 @@
+package com.alibaba.dao;
+
+public class TestJava {
+
+public void printHello(){
+	System.out.println("你好");
+	
+}
+
+}
---
+++ src/com/alibaba/dao/TestJavaRun.java
@@ -0,0 +1,11 @@
+package com.alibaba.dao;
+
+public class TestJavaRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+TestJava testJava = new TestJava();
+testJava.printHello();
+	}
+
+}
